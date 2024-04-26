644f04b1fb04d42ba348eb8f710d18c01ca780d6
Vibodh Bhosure <vibodhbhoure@gmail.com>
2024-04-26 16:15:39 +0530

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "cowhealthnewregionbde2ff27": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
+
+import Sensors from './components/sensorData';
+
+import { Amplify, Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { withAuthenticator } from '@aws-amplify/ui-react'; // or 'aws-amplify-react-native';
+import '@aws-amplify/ui/dist/styles.css';
+
+Amplify.configure(awsconfig);
+
+Auth.currentCredentials().then(creds => console.log(creds));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container className="p-4">
+        <Row className="p-3 justify-content-md-center">
+          <Col md="auto"> <Sensors name="temperature" unit="°F" /> </Col>
+          <Col md="auto"> <Sensors name="Humidity" unit="%" /> </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App, true);
